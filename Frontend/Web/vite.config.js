cf88f4b0da13d8e0af1c958f9641a76fd203e312
Cece <choush@msudenver.edu>
2026-04-05 05:25:44 -0600

    Needed base change for routing

--- Frontend/Web/vite.config.js
+++ Frontend/Web/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lattice/',
+  base: '/', // Changed from '/lattice/' to '/'
   plugins: [react()],
 })
